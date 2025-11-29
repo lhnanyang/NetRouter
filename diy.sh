d3c17c6dd58c93b2fa1c72135323faa64dbc7487
lhnanyang <lhnanyang@163.com>
2025-11-29 14:12:23 +0800

    Create diy.sh to clone OpenWrt packages
    Added a script to clone multiple OpenWrt packages.

---
+++ diy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git openwrt/package/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2.git openwrt/package/passwall2
+
+# SSR+ / Xray
+git clone https://github.com/fw876/helloworld.git openwrt/package/helloworld
+
+# OpenClash
+git clone https://github.com/vernesong/OpenClash.git openwrt/package/openclash
+
+# Docker
+git clone https://github.com/lisaac/luci-app-dockerman.git openwrt/package/dockerman
+
+# Homeproxy
+git clone https://github.com/immortalwrt/homeproxy.git openwrt/package/homeproxy
