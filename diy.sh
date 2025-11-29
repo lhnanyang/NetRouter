ffe8418eb4dec9d2adddeac13b5c04c0bfdc42f3
lhnanyang <lhnanyang@163.com>
2025-11-29 17:38:35 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,16 +2,16 @@
 
 # Passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git openwrt/package/passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2.git openwrt/package/passwall2
+#git clone https://github.com/xiaorouji/openwrt-passwall2.git openwrt/package/passwall2
 
 # SSR+ / Xray
-git clone https://github.com/fw876/helloworld.git openwrt/package/helloworld
+#git clone https://github.com/fw876/helloworld.git openwrt/package/helloworld
 
 # OpenClash
 git clone https://github.com/vernesong/OpenClash.git openwrt/package/openclash
 
 # Docker
-git clone https://github.com/lisaac/luci-app-dockerman.git openwrt/package/dockerman
+#git clone https://github.com/lisaac/luci-app-dockerman.git openwrt/package/dockerman
 
 # Homeproxy
-git clone https://github.com/immortalwrt/homeproxy.git openwrt/package/homeproxy
+#git clone https://github.com/immortalwrt/homeproxy.git openwrt/package/homeproxy
